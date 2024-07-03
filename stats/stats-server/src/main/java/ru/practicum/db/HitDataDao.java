17c19463eab669cef01e5653db94964f9cbcb391
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-07-03 17:02:20 +0300

    commit 2 (stat_svc) - исправления по комментариям ревью

--- stats/stats-server/src/main/java/ru/practicum/db/HitDataDao.java
+++ stats/stats-server/src/main/java/ru/practicum/db/HitDataDao.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import ru.practicum.db.model.DbHitData;
-import ru.practicum.model.HitOutput;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -28,15 +26,5 @@ public class HitDataDao {
 
     public void saveHit(DbHitData hitData) {
         statsRepo.save(hitData);
-    }
-
-    public List<HitOutput> getHitOutput(LocalDateTime startTime,
-                                        LocalDateTime endTime,
-                                        List<String> uris,
-                                        boolean unique,
-                                        String appName) {
-        Specification<DbHitData> specification = Specification
-                .where(HitSpec.getHitOutput(startTime, endTime, uris, unique, appName));
-        return List.of();
     }
 }
