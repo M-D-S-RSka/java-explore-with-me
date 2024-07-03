cb002c56b53a50e10a81fad634e39e81a3a19168
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-07-03 19:45:23 +0300

    3 этап корректировки после ревью

--- stats/stats-server/src/main/java/ru/practicum/rest/StatsRestController.java
+++ stats/stats-server/src/main/java/ru/practicum/rest/StatsRestController.java
@@ -3,11 +3,11 @@ package ru.practicum.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.model.HitInput;
 import ru.practicum.model.HitOutput;
 import ru.practicum.service.StatsService;
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class StatsRestController {
     private final StatsService service;
--- stats/stats-server/src/main/java/ru/practicum/service/HitsMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/service/HitsMapper.java
@@ -3,10 +3,11 @@ package ru.practicum.service;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.springframework.stereotype.Component;
 import ru.practicum.db.model.DbHitData;
 import ru.practicum.model.HitInput;
 
-//@Component
+@Component
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, injectionStrategy = InjectionStrategy.FIELD)
 public interface HitsMapper {
     DbHitData fromInput(HitInput hitInput);
