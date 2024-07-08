8e2529f31693e348dcefefd8a9a4d2ee72081a5f
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-07-08 14:41:22 +0300

    1 этап корректировки после второго ревью

--- stats/stats-server/src/main/java/ru/practicum/rest/StatsRestController.java
+++ stats/stats-server/src/main/java/ru/practicum/rest/StatsRestController.java
@@ -2,19 +2,13 @@ package ru.practicum.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.model.HitInput;
 import ru.practicum.model.HitOutput;
 import ru.practicum.service.StatsService;
 
 import javax.validation.Valid;
-import javax.validation.ValidationException;
-import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -24,9 +18,10 @@ public class StatsRestController {
     private final StatsService service;
 
     @PostMapping("/hit")
-    public ResponseEntity<Object> saveHit(@RequestBody @Valid HitInput hitInput) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public String saveHit(@RequestBody @Valid HitInput hitInput) {
         service.saveHit(hitInput);
-        return ResponseEntity.created(URI.create("/hit")).build();
+        return "Информация сохранена";
     }
 
     @GetMapping("/stats")
@@ -34,10 +29,6 @@ public class StatsRestController {
                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                    @RequestParam(defaultValue = "") List<String> uris,
                                    @RequestParam(defaultValue = "false") boolean unique) {
-        try {
-            return service.getHits(start, end, uris, unique);
-        } catch (IllegalArgumentException e) {
-            throw new ValidationException("Wrong time format");
-        }
+        return service.getHits(start, end, uris, unique);
     }
 }
