7ffa9d611c4766ae8bf3d8a8df9e5c7f2e079631
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-07-17 19:59:37 +0300

    commit 1 main_svc

--- stats/stats-server/src/main/java/ru/practicum/rest/StatsRestController.java
+++ stats/stats-server/src/main/java/ru/practicum/rest/StatsRestController.java
@@ -3,32 +3,43 @@ package ru.practicum.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.model.HitInput;
 import ru.practicum.model.HitOutput;
 import ru.practicum.service.StatsService;
 
 import javax.validation.Valid;
+import javax.validation.ValidationException;
+import java.net.URI;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class StatsRestController {
     private final StatsService service;
+    private final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @PostMapping("/hit")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String saveHit(@RequestBody @Valid HitInput hitInput) {
+    public ResponseEntity<Object> saveHit(@RequestBody @Valid HitInput hitInput) {
         service.saveHit(hitInput);
-        return "Информация сохранена";
+        return ResponseEntity.created(URI.create("/hit")).build();
     }
 
     @GetMapping("/stats")
-    public List<HitOutput> getHits(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                                   @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
-                                   @RequestParam(defaultValue = "") List<String> uris,
-                                   @RequestParam(defaultValue = "false") boolean unique) {
-        return service.getHits(start, end, uris, unique);
+    public List<HitOutput> getHits(@RequestParam String start,
+                                   @RequestParam String end,
+                                   @RequestParam(required = false, defaultValue = "") List<String> uris,
+                                   @RequestParam(required = false, defaultValue = "false") boolean unique) {
+
+        try {
+            var startTime = LocalDateTime.parse(start, dtf);
+            var endTime = LocalDateTime.parse(end, dtf);
+            return service.getHits(startTime, endTime, uris, unique);
+        } catch (IllegalArgumentException e) {
+            throw new ValidationException("Wrong time format");
+        }
     }
 }
