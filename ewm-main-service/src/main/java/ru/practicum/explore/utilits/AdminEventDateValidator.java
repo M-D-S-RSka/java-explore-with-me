03306f9cb5773ae86a81fef6a389c40c0358ca60
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-07-24 14:53:52 +0300

    commit 2 main_svc - добавил логирование вместо sout

--- ewm-main-service/src/main/java/ru/practicum/explore/utilits/AdminEventDateValidator.java
+++ ewm-main-service/src/main/java/ru/practicum/explore/utilits/AdminEventDateValidator.java
@@ -1,10 +1,17 @@
 package ru.practicum.explore.utilits;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDateTime;
 
+@Slf4j
 public class AdminEventDateValidator implements ConstraintValidator<AdminEventDate, LocalDateTime> {
+    private static final Logger logger = LoggerFactory.getLogger(AdminEventDateValidator.class);
+
     @Override
     public void initialize(AdminEventDate constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
@@ -13,9 +20,10 @@ public class AdminEventDateValidator implements ConstraintValidator<AdminEventDa
     @Override
     public boolean isValid(LocalDateTime localDateTime, ConstraintValidatorContext constraintValidatorContext) {
         try {
-            System.out.println(localDateTime);
+            logger.info("Validating event date: {}", localDateTime);
             return localDateTime == null || localDateTime.isAfter(LocalDateTime.now().plusHours(1));
         } catch (Exception e) {
+            logger.error("An error occurred while validating the event date", e);
             return false;
         }
     }
