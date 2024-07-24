bd811b34a10d41e64357e8797121884ac947e756
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-07-24 14:20:40 +0300

    commit 2 main_svc - erorrHandler

--- ewm-main-service/src/main/java/ru/practicum/explore/rest/ErrorHandler.java
+++ ewm-main-service/src/main/java/ru/practicum/explore/rest/ErrorHandler.java
@@ -34,4 +34,11 @@ public class ErrorHandler {
         log.error(e.getMessage(), e);
         return Map.of("error", e.getMessage());
     }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> allThrowable(final Exception e) {
+        log.error(e.getMessage(), e);
+        return Map.of("error", e.getMessage());
+    }
 }
